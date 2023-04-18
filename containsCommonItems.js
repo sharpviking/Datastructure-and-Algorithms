e6fd91a3b573633e4f9b7dbb17eba362d75a947e
Abhishek Anand <anandabhishek3607@gmail.com>
2023-04-19 00:51:51 +0530

    19th April commit

--- containsCommonItems.js
+++ containsCommonItems.js
@@ -1,20 +1,20 @@
-const array0 = ['a', 'b', 'c', 'x', 'z']
-const array01 = ['e', 'g', 'j', 'x', 'w']
+// const array0 = ['a', 'b', 'c', 'x', 'z']
+// const array01 = ['e', 'g', 'j', 'x', 'w']
 
 
-function containsCommonItems(arr1, arr2) {
-    for (let i = 0; i < arr1.length; i++) {
-        for (let j = 0; j < arr2.length; j++) {
-            if (arr1[i] === arr2[j]) {
-                return true;
-            }
-        }
-    }
+// function containsCommonItems(arr1, arr2) {
+//     for (let i = 0; i < arr1.length; i++) {
+//         for (let j = 0; j < arr2.length; j++) {
+//             if (arr1[i] === arr2[j]) {
+//                 return true;
+//             }
+//         }
+//     }
 
-    return false;
-}
+//     return false;
+// }
 
-containsCommonItems(array0, array01);
+// containsCommonItems(array0, array01);
 
 
 // Another solution
@@ -26,5 +26,20 @@ const array2 = ["z", "y", "a"];
 
 
 function containsCommonItems2(arr1, arr2) {
-
+    let map = {};
+    for (let i = 0; i < arr1.length; i++) {
+        if (!map[array1[i]]) {
+            const item = array1[i];
+            map[item] = true;
+        }
+    }
+    for (let j = 0; j < arr2.length; j++) {
+        if (map[array2[j]]) {
+            return true;
+        }
+    }
+    return false;
 }
+
+
+containsCommonItems2(array1, array2)
