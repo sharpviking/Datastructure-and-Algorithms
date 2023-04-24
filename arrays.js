5561477e67df29fc9cdc28d81acee3664e2d2f42
Abhishek Anand <anandabhishek3607@gmail.com>
2023-04-25 00:58:57 +0530

    arrays as data structure

---
+++ arrays.js
@@ -0,0 +1,17 @@
+const string = ['a', 'b', 'c', 'd', 'e']
+
+//push
+string.push('z'); //0(1)
+
+//pop
+string.pop(); // 0(n)
+
+//unshift
+
+string.unshift('A'); // 0(n)
+
+//splice
+
+string.splice(2, 0, 'alien'); // 0(n)
+
+console.log(string);
