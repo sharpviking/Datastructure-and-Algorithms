e56a58da87e655abf5494173aae82ed5c3f95fc3
Abhishek Anand <anandabhishek3607@gmail.com>
2023-06-02 14:15:57 +0530

    Intro to Hash Tables

---
+++ hash_tables.js
@@ -0,0 +1,15 @@
+let user = {
+    name: "John Wick",
+    age: 57,
+    art: "Gunfu",
+    scream: function () {
+        console.log('Yeah...')
+    }
+
+}
+
+user.age //0(1)
+user.spell = 'Boogeyman';
+
+const a = new Map()
+const b = new Set()
