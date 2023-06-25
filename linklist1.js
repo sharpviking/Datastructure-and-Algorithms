9415e82cb779dd4d9b654ec1e53f259dd457c825
Abhishek Anand <anandabhishek3607@gmail.com>
2023-06-26 00:55:53 +0530

    another commit

--- linklist1.js
+++ linklist1.js
@@ -51,10 +51,69 @@ class LinkedList {
         this.length++;
         return this;
     }
+
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return array;
+    }
+
+    insert(index, value) {
+        if (index >= this.length) {
+            return this.append(value);
+        }
+        const newNode = {
+            vlue: value,
+            next: null
+        };
+        const leader = this.traverseToIndex(index - 1)
+        const holdingPointer = leader.next
+        leader.next = newNode;
+        newNode.next = holdingPointer;
+        this.length++;
+        return this.printList();
+
+    }
+    traverseToIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+        while (counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
+    }
+
+    remove(index) {
+        const leader = this.traverseToIndex(index - 1);
+        const unwantedNode = leader.next;
+        leader.next = unwantedNode.next;
+        this.length--;
+        return this.printList();
+    }
+
 }
 
 const myLinkedList = new LinkedList(10)
 myLinkedList.append(11);
 myLinkedList.append(26);
 myLinkedList.prepend(1993)
-console.log(myLinkedList);
+myLinkedList.printList();
+myLinkedList.insert(2, 11)
+myLinkedList.remove(2);
+// console.log(myLinkedList);
+
+
+
+
+
+
+//  Insert BugFix
+// if (index === 0) {
+//     this.prepend(value);
+//     return this.printList();
+//   }
