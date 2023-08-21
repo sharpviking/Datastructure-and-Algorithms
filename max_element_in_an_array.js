6634b9feffb6cdb9d10285ba840914f6488e7f3e
Abhishek Anand <anandabhishek3607@gmail.com>
2023-08-21 15:47:06 +0530

    max number in an array

---
+++ max_element_in_an_array.js
@@ -0,0 +1,14 @@
+function findMax(arr) {
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+
+const numbers = [3, 9, 15, 11, 17];
+console.log(findMax(numbers))
